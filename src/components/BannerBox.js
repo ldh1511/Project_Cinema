d8b972eac64206b01d94d7e43cddc7dcf4c37d98
ldh1511 <53638313+ldh1511@users.noreply.github.com>
2021-03-07 10:52:30 +0700

    update responsive

--- src/components/BannerBox.js
+++ src/components/BannerBox.js
@@ -3,6 +3,7 @@ import banner from './../img/banner01.jpg';
 const BannerBox = () => {
     return (
         <div className='banner-box'>
+            <h1>it's movie time!</h1>
             <img src={banner} alt=""></img>
         </div>
     )
